818fdb87d5dfc1f55d5eb59ead15e9cff0a52544
gsmall22 <gsmall22@s22gsmall.fios-router.home>
2021-12-13 17:26:29 -0500

    line additions

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,14 +2,6 @@ const cells = [];
 let numberOfMoves = 0;
 const gameboard = document.getElementById("gameboard")
 document.getElementById("reset").addEventListener("click", reset, false);
-function reset(){
-    numberOfMoves = 0;
-    for(const cell of gameboard.getElementsByClassName("cell")){
-        cell.textContent = "";
-        cell.addEventListener("click", onclick, false);
-    }
-    document.getElementById("result").textContent = "";
-}
 for (const row of gameboard.getElementsByClassName("row")) {
     const rowArray = [];
     for (const cell of row.getElementsByClassName("cell")) {
@@ -18,8 +10,42 @@ for (const row of gameboard.getElementsByClassName("row")) {
     }
     cells.push(rowArray)
 }
+const firstRow = gameboard.getElementsByClassName("row")[0];
+const secondRow = gameboard.getElementsByClassName("row")[1];
+const thirdRow = gameboard.getElementsByClassName("row")[2];
+
+function reset(){
+    numberOfMoves = 0;
+    for(const cell of gameboard.getElementsByClassName("cell")){
+        cell.textContent = "";
+        cell.addEventListener("click", onclick, false);
+    }
+    document.getElementById("result").textContent = "";
+}
+
 const winningCombinations = [
-    [cells[0][0], cells[0][1], cells[0][2]], 
+    {
+        cells: [cells[0][0], cells[0][1], cells[0][2]],
+        x1: 0,
+        y1: firstRow.offsetHeight/2,
+        x2: firstRow.offsetWidth,
+        y2: firstRow.offsetHeight/2,
+    },
+    {
+        cells: [cells[1][0], cells[1][1], cells[1][2]],
+        x1: 0,
+        y1: firstRow.offsetHeight + secondRow.offsetHeight/2,
+        x2: secondRow.offsetWidth,
+        y2: firstRow.offsetHeight/2 + secondRow.offsetHeight/2,
+    },
+    {
+        cells: [cells[2][0], cells[2][1], cells[2][2]],
+        x1: 0,
+        y1: firstRow.offsetHeight + secondRow.offsetHeight + thirdRow.offsetHeight/2,
+        x2: thirdRow.offsetWidth,
+        y2: firstRow.offsetHeight + secondRow.offsetHeight + thirdRow.offsetHeight/2,
+    },
+
     [cells[1][0], cells[1][1], cells[1][2]],
     [cells[2][0], cells[2][1], cells[2][2]],
     [cells[0][0], cells[1][0], cells[2][0]],
